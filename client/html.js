f5ac6eeb1085d87af71c698424ca2376897a4e8c
Paul Rodwell <paul.rodwell@btinternet.com>
2026-05-14 11:26:04 +0100

    add svg download and zoom

--- client/html.js
+++ client/html.js
@@ -76,6 +76,9 @@ builtins = {
 }
 
 async function emit($item, item) {
+  if (!$("link[href='/plugins/html/html.css']").length) {
+    $('<link rel="stylesheet" href="/plugins/html/html.css" type="text/css">').appendTo('head')
+  }
   await dependencyLoaded
   function sanitize(dirty) {
     return window.DOMPurify.sanitize(dirty, {
@@ -148,11 +151,138 @@ a.external::after {content:url(/images/external-link-ltr-icon.png);}
   })
 }
 
-function bind($item, item) {}
+function bind($item, item) {
+  if ($item[0].querySelectorAll('svg').length > 0) {
+    const svgs = $item[0].querySelectorAll('svg')
+
+    const download = (filename, text) => {
+      const element = document.createElement('a')
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+      element.setAttribute('download', filename)
+      element.style.display = 'none'
+      document.body.appendChild(element)
+      element.click()
+      document.body.removeChild(element)
+    }
+
+    const svgClickHandler = event => {
+      const { target } = event
+      const action = (target.closest('a') || {}).dataset?.action
+
+      if (!action) return
+
+      event.stopPropagation()
+      event.preventDefault()
+      const svg = action ? event.originalTarget.closest('.wrappedSVG').querySelector('svg').outerHTML : ''
+      switch (action) {
+        case 'download':
+          const slug = $item.parents('.page').attr('id')
+          download(`${slug}.svg`, svg)
+          break
+        case 'zoom':
+          const pageKey = $item.parents('.page').data('key')
+          const context = wiki.lineup.atKey(pageKey).getContext()
+          const htmlDialog = window.open(
+            '/plugins/html/dialog/#',
+            event.shiftKey ? '_blank' : 'html',
+            'popup,height=600,width=800',
+          )
+          //const svg = event.originalTarget.closest('.wrappedSVG').querySelector('svg').outerHTML
+          if (htmlDialog.location.pathname !== '/plugins/html/dialog/') {
+            htmlDialog.addEventListener('load', event => {
+              htmlDialog.postMessage({ svg: svg, pageKey, context }, window.origin)
+            })
+          } else {
+            htmlDialog.postMessage({ svg: svg, pageKey, context }, window.origin)
+          }
+          break
+      }
+    }
+
+    svgs.forEach(svg => {
+      console.log(svg)
+      const wrapper = document.createElement('div')
+      wrapper.classList.add('wrappedSVG')
+      svg.parentNode.insertBefore(wrapper, svg)
+      wrapper.appendChild(svg)
+      const overlay = document.createElement('nav')
+      overlay.setAttribute('class', 'actions')
+
+      const downloadAnchor = Object.assign(document.createElement('a'), {
+        href: '#',
+        title: 'Download',
+      })
+      downloadAnchor.dataset.action = 'download'
+      const downloadImg = Object.assign(document.createElement('img'), {
+        width: 18,
+        height: 18,
+        alt: 'download',
+        src: 'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="grey"><g><rect fill="none" height="24" width="24"/></g><g><path d="M5,20h14v-2H5V20z M19,9h-4V3H9v6H5l7,7L19,9z"/></g></svg>',
+      })
+      downloadAnchor.append(downloadImg)
+
+      const zoomAnchor = Object.assign(document.createElement('a'), {
+        href: '#',
+        title: 'Zoom',
+      })
+      zoomAnchor.dataset.action = 'zoom'
+      const zoomImg = Object.assign(document.createElement('img'), {
+        width: 18,
+        height: 18,
+        alt: 'toggle zoom',
+        src: 'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path fill="grey" d="M15,3l2.3,2.3l-2.89,2.87l1.42,1.42L18.7,6.7L21,9V3H15z M3,9l2.3-2.3l2.87,2.89l1.42-1.42L6.7,5.3L9,3H3V9z M9,21 l-2.3-2.3l2.89-2.87l-1.42-1.42L5.3,17.3L3,15v6H9z M21,15l-2.3,2.3l-2.87-2.89l-1.42,1.42l2.89,2.87L15,21h6V15z"/></g></g></g></svg>',
+      })
+      zoomAnchor.append(zoomImg)
+
+      overlay.append(downloadAnchor, zoomAnchor)
+
+      console.log('overlay', overlay)
+      svg.parentNode.insertBefore(overlay, svg)
+      wrapper.addEventListener('click', svgClickHandler)
+    })
+  }
+}
+
+function htmlListener(event) {
+  // only continue if event is from a graphviz popup.
+  // events from a popup window will have an opener
+  // ensure that the popup window is one of ours
+  if (!event.source.opener || event.source.location.pathname !== '/plugins/html/dialog/') {
+    if (wiki.debug) {
+      console.log('htmlListener - not for us', { event })
+    }
+    return
+  }
+  if (wiki.debug) {
+    console.log('htmlListener - ours', { event })
+  }
+
+  const { data } = event
+  const { action, keepLineup = false, pageKey = null, title = null, context = null } = data
+
+  let $page = null
+  if (pageKey != null) {
+    $page = keepLineup ? null : $('.page').filter((i, el) => $(el).data('key') == pageKey)
+  }
+
+  switch (action) {
+    case 'doInternalLink':
+      wiki.pageHandler.context = context
+      wiki.doInternalLink(title, $page)
+      break
+    default:
+      console.error({ where: 'htmlListener', message: 'unknown action', data })
+  }
+}
 
 if (typeof window !== 'undefined' && window !== null) {
   window.plugins.html = {
     emit: emit,
     bind: bind,
+  }
+  if (typeof window.htmlListener !== 'undefined' || window.htmlListener == null) {
+    console.log('**** Adding html listener')
+    window.htmlListener = htmlListener
+    window.addEventListener('message', htmlListener)
   }
 }
